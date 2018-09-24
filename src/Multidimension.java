29d92f97147890fd5ab4dcadafd2eaef4c5e4cdd
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-25 00:20:51 +0200

    multidimension2

--- src/Multidimension.java
+++ src/Multidimension.java
@@ -13,5 +13,20 @@ public class Multidimension {
         System.out.println(firstLast[0][2] + " " + firstLast[1][2]);
         System.out.println("Czwarta osoba:");
         System.out.println(firstLast[0][3] + " " + firstLast[1][3]);
+
+        int[] longTab = new int[1000];
+        longTab[99] = 100;
+        int[] midTab = new int[100];
+        int[] smallTab = new int[10];
+
+        int[][] hugeTab = new int[3][];
+        hugeTab[0] = longTab;
+        hugeTab[1] = midTab;
+        hugeTab[2] = smallTab;
+
+        System.out.println("Długości tablic: ");
+        System.out.println(hugeTab[0].length + " ; " + hugeTab[1].length + " ; " + hugeTab[2].length);
+        System.out.println("Setny element pierwszej tablicy: " + hugeTab[0][99]);
+
     }
 }
