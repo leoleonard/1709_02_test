8928945ae4d8c73aafbaf3b15dfb4c76c69bef59
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-24 21:27:10 +0200

    add comment out konstruktor

--- src/Computer.java
+++ src/Computer.java
@@ -2,10 +2,10 @@ public class Computer {
     String processor;
     int memory;
 
-    Computer(String proc, int mem) {
-        processor = proc;
-        memory = mem;
-    }
+//    Computer(String proc, int mem) {
+//        processor = proc;
+//        memory = mem;
+//    }
 
     void printInfo() {
         System.out.println(processor + " " + memory);
--- src/ComputerStore.java
+++ src/ComputerStore.java
@@ -1,45 +1,45 @@
 public class ComputerStore {
     public static void main(String[] args) {
-//        Computer comp1 = new Computer();
-//        comp1.processor = "Intel";
-//        comp1.memory = 1224;
-//
-//        Computer comp2 = new Computer();
-//        comp2.processor = "AMD";
-//        comp2.memory = 812;
-//
-//        comp1.printInfo();
-//        comp2.printInfo();
-//
-//        ComputerUpgrade upgrade = new ComputerUpgrade();
-//        upgrade.addMemory(comp1, 100);
-//
-//        ComputerUpgrade upgrade2 = new ComputerUpgrade();
-//        upgrade.addMemory(comp2, 200);
-//
-//        comp1.printInfo();
-//        comp2.printInfo();
+        Computer comp1 = new Computer();
+        comp1.processor = "Intel";
+        comp1.memory = 1224;
 
-//        ComputerFactory factory = new ComputerFactory();
-//        Computer comp3 = factory.create("Intel2", 827);
-//        Computer comp4 = factory.create("AMD2", 112);
-//
-//        comp3.printInfo();
-//        comp4.printInfo();
-//
-//        ComputerUpgrade upgrade3 = new ComputerUpgrade();
-//        upgrade3.addMemory(comp3, 100);
-//        upgrade3.addMemory(comp4, 30);
-//
-//        comp3.printInfo();
-//        comp4.printInfo();
+        Computer comp2 = new Computer();
+        comp2.processor = "AMD";
+        comp2.memory = 812;
 
-        Computer comp5 = new Computer("New Intel", 512);
+        comp1.printInfo();
+        comp2.printInfo();
 
-        ComputerUpgrade upgrade4 = new ComputerUpgrade();
-        upgrade4.addMemory(comp5, 512);
+        ComputerUpgrade upgrade = new ComputerUpgrade();
+        upgrade.addMemory(comp1, 100);
 
-        comp5.printInfo();
+        ComputerUpgrade upgrade2 = new ComputerUpgrade();
+        upgrade.addMemory(comp2, 200);
+
+        comp1.printInfo();
+        comp2.printInfo();
+
+        ComputerFactory factory = new ComputerFactory();
+        Computer comp3 = factory.create("Intel2", 827);
+        Computer comp4 = factory.create("AMD2", 112);
+
+        comp3.printInfo();
+        comp4.printInfo();
+
+        ComputerUpgrade upgrade3 = new ComputerUpgrade();
+        upgrade3.addMemory(comp3, 100);
+        upgrade3.addMemory(comp4, 30);
+
+        comp3.printInfo();
+        comp4.printInfo();
+
+//        Computer comp5 = new Computer("New Intel", 512);
+//
+//        ComputerUpgrade upgrade4 = new ComputerUpgrade();
+//        upgrade4.addMemory(comp5, 512);
+//
+//        comp5.printInfo();
 
     }
 }
